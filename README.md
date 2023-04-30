b1040bd76a3cbc6ef40f491965d6c0b8374f1833
MinJee Kim <hbmj1017@naver.com>
2023-04-30 22:09:44 +0900

    Finish clock component and switch

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-logo 관련 이미지 출처
+## 주요 구현 기능
+1. 시계
+2. 타이머
+3. 오늘 날씨
+4. 미세먼지
+5. 오늘의 명언
+6. to-do-list ( 오늘 목표, to-do)
+7. 일기
+8. 주요 링크 모음
+9. 로그아웃
+10. 페이지 설정
+
+
+
+## logo 관련 이미지 출처
 1. https://emblemmatic.org/markmaker/#/
 2. https://www.cleanpng.com/png-meteoroid-meteorite-light-impact-crater-meteor-743905/
---
+++ src/js/clock.js
@@ -0,0 +1,41 @@
+const clock = document.querySelector(".main__aside__clock h1")
+const clockToggleLabel = document.querySelector(".clock__am-pm .toggle-switch")
+const clockToggleBtn = clockToggleLabel.querySelector(".toggle-button")
+const clockToggleHour = clockToggleBtn.querySelector("span")
+
+const ACTIVE = "active"
+
+// 2칸으로 맞춰주기
+const zeroTime = function (num) {
+    const numTime = parseInt(num)
+    let answer = (numTime < 10) ? ('0' + num) : num
+    return answer
+}
+
+//12시간 기준 hour 변환
+const twelveHour = function (num) {
+    let numHour = parseInt(num)
+    let hour = ''
+    let amPm = 'AM'
+    amPm = numHour < 13 ? "AM" : "PM"
+    numHour = numHour < 13 ? numHour : (numHour - 12)
+    hour = numHour < 10 ? ('0' + String(numHour)) : String(numHour)
+    return (amPm + ' ' + hour)
+}
+
+const getClock = function () {
+    const date = new Date()
+    const hour = (clockToggleBtn.classList.contains(ACTIVE)) ? twelveHour(date.getHours()) : zeroTime(date.getHours())
+    const minute = zeroTime(date.getMinutes())
+    const second = zeroTime(date.getSeconds())
+    clock.innerText = `${hour} : ${minute} : ${second}`
+}
+//toggle active 시 변환
+const onClickToggle = function () {    
+    clockToggleBtn.classList.toggle(ACTIVE)
+    clockToggleBtn.innerText = clockToggleBtn.classList.contains(ACTIVE) ? "12H" : "24H"
+}
+
+getClock()
+let twentyFourInterval = setInterval(getClock, 1000)
+clockToggleLabel.addEventListener("click", onClickToggle)
