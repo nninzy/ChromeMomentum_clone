01ca2bf1f5ad2b3d4b060afa2fec9e3ee67c94fb
MinJee Kim <hbmj1017@naver.com>
2023-05-02 03:48:32 +0900

    Create first finished site

--- src/js/backgroundImg.js
+++ src/js/backgroundImg.js
@@ -3,6 +3,14 @@ const bgImages = [
     "https://png.pngtree.com/thumb_back/fh260/background/20220313/pngtree-photography-of-night-scenery-of-galaxy-starry-sky-image_1000896.jpg",
     "https://cdn.pixabay.com/photo/2016/07/17/16/19/stars-1524180_960_720.jpg",
     "https://png.pngtree.com/background/20230317/original/pngtree-beautiful-background-of-starry-sky-at-night-picture-image_2148622.jpg",
+    "https://c.pxhere.com/photos/7e/f0/star_night_sky_meteor_night_sky-2156.jpg!d",
+    "https://img.pikbest.com/back_our/20210228/bg/7f852d3b6f4e4.png!w700wp",
+    "https://img.pikbest.com/back_our/20210301/bg/bcc553997ccfc.png!bw700",
+    "https://newsimg.hankookilbo.com/cms/articlerelease/2021/07/20/c2a06033-1428-47a6-8237-19070e181abd.jpg",
+    "https://www.fnnews.com/resource/media/image/2022/06/20/202206201514075447_l.jpg",
 ]
 
-const chosenBgImg = bgImages[(Math.random()) * bgImages.length]
+const chosenBgImg = bgImages[Math.floor(Math.random() * bgImages.length)]
+const mainBackImage = document.querySelector(".main-bg-image img")
+
+mainBackImage.src = chosenBgImg
--- src/js/todo.js
+++ src/js/todo.js
@@ -2,6 +2,11 @@ const personalTodoList = document.querySelector (".to-do__personal-list ul")
 const personalTodoInputForm = document.querySelector(".to-do__personal-input-form")
 const personalTodoInputText = document.querySelector(".to-do__personal-input-form input")
 
+const deleteTodo = function (event) {
+    const targetli = event.target.parentElement
+    targetli.remove()
+}
+
 const paintPersonalTodo = function (todoContent) {
     const personalTodoItem = document.createElement("li")
     personalTodoItem.classList.add("to-do__personal-list__item")
@@ -12,17 +17,19 @@ const paintPersonalTodo = function (todoContent) {
     const personalTodoDeleteBtn = document.createElement("button")
     const trashIcon = document.createElement("i")
     trashIcon.classList.add("fa-solid", "fa-trash-can-arrow-up")
+    personalTodoDeleteBtn.addEventListener("click", deleteTodo)
     personalTodoDeleteBtn.appendChild(trashIcon)
     personalTodoItem.appendChild(personalTodoCheckbox)
     personalTodoItem.appendChild(personalTodoSpan)
     personalTodoItem.appendChild(personalTodoDeleteBtn)
     personalTodoList.appendChild(personalTodoItem)
 }
+
 const addPersonalTodo = function (event) {
     event.preventDefault()
     const personalTodoContent = personalTodoInputText.value
     personalTodoInputText.value = ""
     paintPersonalTodo(personalTodoContent)
 }
-personalTodoInputForm.addEventListener("submit", addPersonalTodo)
 
+personalTodoInputForm.addEventListener("submit", addPersonalTodo)
